40d106a8db66c332e5d189de55f4a1f6533ddc45
Mikael <mikael.linhares@gmail.com>
2024-04-14 13:31:28 -0300

    Criação Banco de Dados

--- source/models.py
+++ source/models.py
@@ -3,28 +3,25 @@ from django.db import models
 
 class Propriedade(models.Model):
     nome = models.CharField(max_length=50)
+    pessoas = models.ManyToManyField('Pessoa')
 
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=50)
     saldo = models.DecimalField(max_digits=15, decimal_places=2, default=0)
     nascimento = models.DateField(null=True)
-
-
-class PropriedadePessoa(models.Model):
-    id_propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE)
-    id_pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    propriedades = models.ManyToManyField('Propriedade')
 
 
 class Renda(models.Model):
-    id_pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     valor = models.DecimalField(max_digits=15, decimal_places=2)
     descricao = models.TextField(max_length=255)
 
 
 class Meta(models.Model):
-    id_propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE)
-    id_pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE, null=True, blank=False)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, null=True, blank=False)
     valor = models.DecimalField(max_digits=15, decimal_places=2)
     saldo = models.DecimalField(max_digits=15, decimal_places=2)
     descricao = models.TextField(max_length=255)
@@ -38,15 +35,15 @@ class Categoria(models.Model):
 
 
 class Despesa(models.Model):
-    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    id_propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE)
-    id_meta = models.ForeignKey(Meta, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE, null=True, blank=False)
+    id_meta = models.ForeignKey(Meta, on_delete=models.CASCADE, null=True, blank=False)
     valor = models.DecimalField(max_digits=15, decimal_places=2)
     data = models.DateTimeField()
 
 
 class Pagamento(models.Model):
-    id_despesa = models.ForeignKey(Despesa, on_delete=models.CASCADE)
-    id_pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    despesa = models.ForeignKey(Despesa, on_delete=models.CASCADE)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     valor = models.DecimalField(max_digits=15, decimal_places=2)
     data = models.DateTimeField()
