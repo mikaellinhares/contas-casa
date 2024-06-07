546adfeae9e9bd02184aec6d0482685cd5f3deb0
Mikael <mikael.linhares@gmail.com>
2024-06-07 16:59:02 -0300

    v0.0.1

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-pip install -r requirements.txt
-python3.10 manage.py migrate
-python3.10 manage.py collectstatic
+echo "BUILD START"
+python3.9 -m ensurepip 
+python3.9 -m pip install -r requirements.txt 
+python3.9 manage.py collectstatic --noinput --clear 
+echo "BUILD END"
